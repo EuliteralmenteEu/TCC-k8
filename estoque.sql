58be2263656204692cb8be3db0c743b22fc22cae
LUSKAS <lukasbiasotto710@Gmail.com>
2026-05-27 08:16:26 -0300

    Add files via upload
    sql de estoque

---
+++ estoque.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE almoxarifado;
+USE almoxarifado;
+
+CREATE TABLE estoque(
+	id INT,
+	nome VARCHAR(32) NOT NULL,
+	quantidade INT,
+	categoria VARCHAR(24) NOT NULL,
+	descrição VARCHAR(64) NOT NULL,
+    PRIMARY KEY (`id`)
+	);
+
+CREATE DATABASE login;
+USE login;
+
+CREATE TABLE usuarios (
+	email VARCHAR(48) NOT NULL,
+    senha VARCHAR(32) NOT NULL,
+    permisao BOOL,
+    PRIMARY KEY (`email`)
+	);
+
+INSERT INTO usuarios (email, senha, permisao) VALUES ('admin@gmail.com', 'admin123', 1)
