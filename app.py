e80d30d69a594c2904d7daead541b1cdf8c13d3e
LUSKAS <lukasbiasotto710@Gmail.com>
2026-06-10 09:13:09 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,12 @@ from flask import Flask
 import mysql.connector 
 import os
 
-
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return "Olá, mundo! Este é o documento padrão do Flask."
+    return "Satanael vai se libertar do senai!"
 
 
 if __name__ == "__main__":
