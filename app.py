dddeff11becfcecd5feb061363055e45d2dce9db
LUSKAS <lukasbiasotto710@Gmail.com>
2026-06-17 08:39:21 -0300

    Use environment variables for database configuration

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, g
 import mysql.connector 
 import os
 
 app = Flask(__name__)
 
 #----------SQL------------------
-DB_HOST = 'localhost'
-DB_USER = 'seu_usuario'
-DB_PASSWORD = 'sua_senha'
-DB_DATABASE = 'seu_banco'
+DB_HOST = os.getenv('DB_HOST','localhost')
+DB_USER = os.getenv('DB_USER','seu_usuario')
+DB_PASSWORD = os.getenv('DB_PASSWORD','sua_senha')
+DB_DATABASE = os.getenv('DB_DATABASE','seu_banco')
 
 def get_db():
     """Abre uma conexão com o banco de dados se já não houver uma ativa na requisição."""
