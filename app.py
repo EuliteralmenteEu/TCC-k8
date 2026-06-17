5b971e3cb3f065ad5905fb36b347ca967b20c29c
LUSKAS <lukasbiasotto710@Gmail.com>
2026-06-17 08:19:16 -0300

    Fix syntax error in app.run parameters

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 import mysql.connector 
 import os
 
@@ -36,4 +36,4 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(debug=True host=0.0.0.0, port=3000)
+    app.run(debug=True, host='0.0.0.0', port=3000)
