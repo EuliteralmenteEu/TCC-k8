e56ff6b12016b1f7e7a2312cf0c264c433c3b3c6
LUSKAS <lukasbiasotto710@Gmail.com>
2026-07-01 08:20:04 -0300

    Remove hardcoded password from thanatos.py

--- thanatos.py
+++ thanatos.py
@@ -10,7 +10,6 @@ password_hash = bytes_hash.decode('utf-8')
 print("Hash gerado:", bytes_hash.decode('utf-8'))
 
 
-senha_digitada = "minha_senha_comida"
 
 if bcrypt.checkpw(senha_digitada.encode('utf-8'), bytes_hash):
     print("Sucesso: A senha está correta!")
